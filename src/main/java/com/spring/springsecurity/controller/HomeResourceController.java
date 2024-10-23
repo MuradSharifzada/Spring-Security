3a22d8e564f20b0cdff1ff47d3933d17a735304d
Murad Sherifzade <muradsharifzada@gmail.com>
2024-10-23 22:23:16 +0400

    Creating custom authorization page

---
+++ src/main/java/com/spring/springsecurity/controller/HomeResourceController.java
@@ -0,0 +1,24 @@
+package com.spring.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResourceController {
+    @GetMapping("/hello")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Welcome User</h1>");
+    }
+
+
+}
